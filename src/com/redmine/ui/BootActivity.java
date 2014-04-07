d75d11e70528c9aefc4a91fa2c809010d2b2fa0c
hidetzu <hidetzu@gmail.com>
2014-04-07 21:32:41 +0900

    インストールイベントを拾うようにした。

--- src/com/redmine/ui/BootActivity.java
+++ src/com/redmine/ui/BootActivity.java
@@ -19,25 +19,20 @@ public class BootActivity extends Activity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		createDesktopShortCut(this);
+		
 		mBootPresenter = new BootPresenter(this, AcountDatabaseHelper.getInstance(this));
-		createDesktopShorCut(this);
 		mBootPresenter.moveToLoginView();
 	}
-	
-	private void createDesktopShorCut(Context context) {
-		Intent targetIntent = new Intent(Intent.ACTION_MAIN);
-		targetIntent.setClassName(context, "com.redmine.ui.BootActivity");
-		
-		Intent i = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
-		i.putExtra(Intent.EXTRA_SHORTCUT_INTENT, targetIntent);
-		
-		//2. アイコン画像とタイトルを設定
-		Parcelable icon = Intent.ShortcutIconResource.fromContext(context, R.drawable.ic_launcher);
-		i.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
-		i.putExtra(Intent.EXTRA_SHORTCUT_NAME, getResources().getString(R.string.app_name));
 
-		//3. 1～2で作成したIntentをBroadcastしてOSに依頼
-		context.sendBroadcast(i);
+	private void createDesktopShortCut(Context context) {
+		Intent shortcutIntent = new Intent();
+		shortcutIntent.setClassName("com.redmine.ui", getClass().getName());
+
+		Intent intent = new Intent();
+		intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+		intent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+		sendBroadcast(intent);
 	}
 	
 	@Override
